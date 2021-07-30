c5aed51bcec1361d973344aaa2c939ff4c2287b1
hassin23ayz <hassin23ayz@gmail.com>
2021-07-30 15:17:23 +0600

    fake repository of hard coded users created

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,5 +1,35 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo,
-    otp_app: :rumbl,
-    adapter: Ecto.Adapters.Postgres
+  # use Ecto.Repo,
+  #   otp_app: :rumbl,
+  #   adapter: Ecto.Adapters.Postgres
+
+  @moduledoc """
+  In memory Repository
+  A repository is an API for holding things
+  Here we will be implementing a hard coded database
+  """
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Jose",  username: "elixir", password: "josevalim"},
+      %Rumbl.User{id: "2", name: "Chris", username: "Phoenix", password: "cmcord"},
+      %Rumbl.User{id: "3", name: "Bruce", username: "7langs", password: "redrapids"},
+    ]
+  end
+
+  def all(_), do: []
+
+  #get a map from the list by id key value
+  def get(module, id) do
+    # Enum.find all(module), fn map -> map.id == id end
+    Enum.find(all(module), fn map -> map.id == id end)
+  end
+
+  #get a map from the list by {key, val}
+  def get_by(module, params) do
+    Enum.find(all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val
+      end)
+    end)
+  end
+
 end
---
+++ lib/rumbl/user.ex
@@ -0,0 +1,6 @@
+defmodule Rumbl.User do
+  @moduledoc """
+  This Module is in Model Layer of MVC
+  """
+  defstruct [:id, :name, :username, :password]
+end
